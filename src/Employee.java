ce1f521e61b8e236ff680dc92bc761b70ddd5e8c
Tatyana58 <tatbir@rambler.ru>
2022-12-18 13:05:58 +0300

    Beginning Создали класс Employee Создали конструктор Employee

--- src/Employee.java
+++ src/Employee.java
@@ -1,11 +1,22 @@
-public class Employee {
-    String name;
-    String surname;
-    String patronymic;
-    int department;
-    int wages;
-    int id;
+import java.util.Objects;
 
+public class Employee {
+    private final String  name;
+    private final String surname;
+    private final String patronymic;
+    private int department;
+    private int wages;
+    private int id;
+
+    //конструктор Employee
+    public Employee(String surname, String name, String patronymic, int department, int wages, int id) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.department = department;
+        this.wages = wages;
+        this.id = id;
+    }
     public String getName() {
         return name;
     }
@@ -36,5 +47,22 @@ public class Employee {
 
     public void setWages(int wages) {
         this.wages = wages;
+    }
+
+    @Override
+    public String toString() {
+        return surname + "" + name + "" + patronymic + " из отдела: " + department + " с зп " + wages + " ID " + id;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return department == employee.department && wages == employee.wages && id == employee.id && Objects.equals(name, employee.name) && Objects.equals(surname, employee.surname) && Objects.equals(patronymic, employee.patronymic);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, patronymic, department, wages, id);
     }
 }
