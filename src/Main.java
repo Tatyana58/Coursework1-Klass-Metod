dc3176463bc78571606064917a2d6a8bbd800ea9
Tatyana58 <tatbir@rambler.ru>
2022-12-23 21:45:35 +0300

    Beginning Создали класс Employee Создали конструктор Employee Вывели всех сотрудников в консоль Ветка 2 Считаем общую сумму заплат всех сотрудников по всем отделам

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,39 @@
-import java.util.Arrays;
-
 public class Main {
     static final int N=10;
-    public int id=0;
     public static void main(String[] args) {
         System.out.println("Курсовая работа №1 Классы и методы.");
         Employee[] collegue = new Employee[N];
-        collegue[0]= new Employee("Иванов ","Дмитрий ","Иванович ",1,4000,1 );
-        collegue[1] = new Employee("Сидоров ", "Сергей ", "Петрович ", 2, 1000, 2);
-        collegue[2] = new Employee("Лаврова ", "Любовь ", "Васильевна ", 4, 7000, 4);
-        collegue[3] = new Employee("Макеев ", "Александр ", "Андреевич ", 3, 2000, 4);
-        collegue[4] = new Employee("Юдина ", "Ольга ", "Борисовна ", 1, 3000, 4);
-        collegue[5] = new Employee("Сергеев ", "Юрий ", "Витальевич ", 5, 5000, 4);
-        collegue[6] = new Employee("", "", "", 4, 7000, 4);
-        collegue[7] = new Employee("", "", "", 4, 5000, 4);
-        collegue[8] = new Employee("", "", "", 4, 5000, 4);
-        collegue[9] = new Employee("", "", "", 4, 7000, 4);
+        collegue[0] = new Employee("Иванов ", "Дмитрий ", "Иванович ", 1, 4000);
+        collegue[1] = new Employee("Сидоров ", "Сергей ", "Петрович ", 2, 1000);
+        collegue[2] = new Employee("Лаврова ", "Любовь ", "Васильевна ", 4, 7000);
+        collegue[3] = new Employee("Макеев ", "Александр ", "Андреевич ", 3, 2000);
+        collegue[4] = new Employee("Юдина ", "Ольга ", "Борисовна ", 1, 3000);
+        collegue[5] = new Employee("Сергеев ", "Юрий ", "Витальевич ", 5, 5000);
+        collegue[6] = new Employee("", "", "", 4, 7000);
+        collegue[7] = new Employee("", "", "", 4, 5000);
+        collegue[8] = new Employee("", "", "", 4, 5000);
+        collegue[9] = new Employee("", "", "", 4, 7000);
 
-        //Вывод всех сотрудников
-        for (int x=0;x< collegue.length;x++) {
-            System.out.println(collegue[x]);
+        for (int i = 0; i <N ; i++){
+        Employee.sumWages(collegue[i].getWages());
         }
-        //находим сумму всех зарплат
-        int sumZP=0;
-        int sredSumZP=0;
-        for (int i = 0; i < collegue.length; i++) {
-            sumZP+=collegue[i].getWages();
-        }
-        System.out.println("Сумма всех зарплат - "+sumZP);
-        //находим среднюю зарплату
-        sredSumZP=sumZP/ collegue.length;
-        System.out.println("Средняя зарплата - "+sredSumZP);
 
 
+    /*public static double sumWages(){
+        double sum = 0;
+        sum=+1();
+        return sum;
     }
 
-    int K=0;
 
-    public void totalWages(Employee surname,Employee name, Employee patronymic, int department, double wages, int id)
-    {
-        wages++;
-        id++;
-        return;
+     */}
+
+
+    public static void printInfo(Employee surname, Employee name, Employee patronymic, Employee department, Employee wages,Employee id) {
+        System.out.println("Сотрудники" + surname.getSurname() + "" + name.getName() + "" + patronymic.getPatronymic() + "" + department.getDepartment() + "" + wages.getWages() + "" + id.getId());
     }
+
+
+
+
 }
