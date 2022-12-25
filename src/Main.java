cab76d169b5fdaaaa1e66b9ac5265a467d26c10c
Tatyana58 <tatbir@rambler.ru>
2022-12-25 22:03:59 +0300

    Beginning Создали класс Employee Создали конструктор Employee Вывели всех сотрудников в консоль Ветка 2 Считаем общую сумму заплат всех сотрудников по всем отделам1 Среднее значение зарплат Поиск сотрудника с максимальной заработной платой Строка из массива типа байт

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,8 @@
 public class Main {
-    static final int N=10;
     public static void main(String[] args) {
-        System.out.println("Курсовая работа №1 Классы и методы.");
-        Employee[] collegue = new Employee[N];
-        collegue[0] = new Employee("Иванов ", "Дмитрий ", "Иванович ", 1, 4000);
-        collegue[1] = new Employee("Сидоров ", "Сергей ", "Петрович ", 2, 1000);
-        collegue[2] = new Employee("Лаврова ", "Любовь ", "Васильевна ", 4, 7000);
-        collegue[3] = new Employee("Макеев ", "Александр ", "Андреевич ", 3, 2000);
-        collegue[4] = new Employee("Юдина ", "Ольга ", "Борисовна ", 1, 3000);
-        collegue[5] = new Employee("Сергеев ", "Юрий ", "Витальевич ", 5, 5000);
-        collegue[6] = new Employee("Силантьева ", "ЕКатерина ", "Анатольевна ", 4, 80000);
-        collegue[7] = new Employee("Криулина ", "Евгения ", "Валерьевна ", 4, 15000);
-        collegue[8] = new Employee("Саркисьян ", "Лариса ", "Михайловна ", 4, 300.50);
-        collegue[9] = new Employee("Гречав ", "Максим ", "Павлович ", 4, 6000);
-
-        for (int i = 0; i < collegue.length; i++) {
-            Employee.sumWages(collegue[i].getWages());
-            //Employee.maxWages(collegue[i].getWages());
-        }
-        System.out.println("Сумму затрат на зарплаты " + Employee.sum);
-        System.out.println("Подсчитали среднее значение зарплат: "+Employee.averageWages(Employee.sum));
-
-
+        System.out.println("Срока из массива типа байт");
+        byte[] arr2={115,116,114,105,110,103};
+        String s3=new String(arr2);
+        System.out.println(s3);
     }
-
-
-    public static void printInfo(Employee surname, Employee name, Employee patronymic, Employee department, Employee wages,Employee id) {
-        System.out.println("Сотрудники" + surname.getSurname() + "" + name.getName() + "" + patronymic.getPatronymic() + "" + department.getDepartment() + "" + wages.getWages() + "" + id.getId());
-    }
-
-
-
-
 }
